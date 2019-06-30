e3700ae5a6e6faacd8f7fbbcbd210fd5dcb70313
mammut <jpescalonag@gmail.com>
2019-06-30 17:29:33 -0400

    Add install.sh script to initialize tmux and vim plugin managers. Fix Tmux failure on linux.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install tmux plugins. You need Tmux >= 1.9. prefix, I to install plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install vim Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
